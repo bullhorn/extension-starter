8f55a49bedf835b01cd31e4af917d847129bdcb3
Haris Iqbal <hiqbal@bullhorn.com>
2018-12-21 11:02:24 -0500

    Upgrading to angular 7

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -42,7 +42,7 @@
 
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+
 
 
 /** ALL Firefox browsers require the following to support `@angular/animation`. **/
@@ -70,3 +70,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Need to import at least one locale-data with intl.
  */
 // import 'intl/locale-data/jsonp/en';
+(window as any).global = window;
