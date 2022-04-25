56bbcc5d8b64dc431fcf1f94fc117bd4d329290d
Amr Essam <amressam01@hotmail.com>
2022-04-25 09:07:26 +0200

    upgrade to novo-elements 6

--- src/app/tools/modal/modal.types.ts
+++ src/app/tools/modal/modal.types.ts
@@ -1,32 +1,23 @@
-import {NovoModalParams} from 'novo-elements';
-import {ModalParams} from 'novo-elements/elements/modal/Modal';
+import { ModalParams, NovoModalParams } from 'novo-elements';
+
+export interface CustomModalParams extends ModalParams {
+  message: string;
+  isConfirm: boolean;
+  onClose: (result: boolean) => void;
+}
 
 export class StandardModalParams implements CustomModalParams {
-
   public message: string;
-  public isConfirm: boolean = false;
-  public onClose: (result: boolean) => void = () => {};
+  public isConfirm = false;
 
-  constructor(message: string, isConfirm: boolean = false, onClose: (result: boolean) => void = () => {}) {
+  static fromNovo(params: NovoModalParams): StandardModalParams {
+    return new StandardModalParams(params['message'], params['isConfirm'], params['onClose']);
+  }
+
+  public onClose: (result: boolean) => void = () => { };
+  constructor(message: string, isConfirm: boolean = false, onClose: (result: boolean) => void = () => { }) {
     this.message = message;
     this.isConfirm = isConfirm;
     this.onClose = onClose;
   }
-
-  static fromNovo(params: NovoModalParams): StandardModalParams {
-    return new StandardModalParams(
-      params['message'],
-      params['isConfirm'],
-      params['onClose']
-    );
-  }
-
-}
-
-export interface CustomModalParams extends ModalParams {
-
-  message: string;
-  isConfirm: boolean;
-  onClose: (result: boolean) => void;
-
 }
